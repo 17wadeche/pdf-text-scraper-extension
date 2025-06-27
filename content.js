55198b342328c599c46aa794bc7cf438344407f4
Chey Bronk-Wade <chey.wade@medtronic.com>
2025-06-27 15:15:49 -0500

    now?

--- content.js
+++ content.js
@@ -3,7 +3,7 @@ const ALLOWED_PREFIXES = [
   'https://crm.medtronic.com/sap/bc/contentserver/',
   'https://cpic1cs.corp.medtronic.com:8008/sap/bc/contentserver/'
 ];
-if (!ALLOWED_PREFIXES.some(p => location.href.startsWith(p))) {
+if (ALLOWED_PREFIXES.some(p => location.href.startsWith(p))) {
   (async () => {
     const styleTag = document.createElement('style');
     styleTag.textContent = `
