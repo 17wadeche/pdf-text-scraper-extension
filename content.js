2ddc74ce6a46abf87937e30aa46487edb49c4d69
Chey Bronk-Wade <chey.wade@medtronic.com>
2025-07-08 15:10:39 -0500

    last thing

--- content.js
+++ content.js
@@ -189,6 +189,7 @@ if (ALLOWED_PREFIXES.some(p => location.href.startsWith(p))) {
     document.head.appendChild(fix);
     linkService.setViewer(pdfViewer);
     pdfViewer.setDocument(pdfDoc);
+    pdfViewer.currentScaleValue = 'page-width';
     linkService.setDocument(pdfDoc, null);
     fix.textContent = `
       .textLayer, .textLayer div {
