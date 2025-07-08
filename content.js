2c0cf8d0d5aa98e36b85d6a6e12336bde10cfe88
Chey Bronk-Wade <chey.wade@medtronic.com>
2025-07-08 18:59:04 -0500

    render

--- content.js
+++ content.js
@@ -210,6 +210,9 @@ async function main() {
   pdfViewer.currentScaleValue = 'page-width';
   linkService.setDocument(pdfDoc, null);
   renderAllHighlights();
+  eventBus.on('pagesloaded', () => {
+    renderAllHighlights();
+  });
   eventBus.on('textlayerrendered', ({ pageNumber }) => {
     const pageView  = pdfViewer._pages[pageNumber - 1];
     const textLayer = pageView?.textLayer?.textLayerDiv;
